5443c488dcba87afd01e3d79c1cd4978e7c0a9e2
adityaslyf <aditya.varshneymail@gmail.com>
2025-01-01 20:05:36 +0530

    fix build errors

--- app/(create)/create_collective/Review.tsx
+++ app/(create)/create_collective/Review.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import Image from 'next/image'
+// import Image from 'next/image'
 
 interface ReviewProps {
   squadDetails: {
@@ -16,7 +16,7 @@ interface ReviewProps {
   onConfirm: () => void;
 }
 
-const Review = ({ squadDetails, memberDetails, onBack, onConfirm }: ReviewProps) => {
+const Review = ({ memberDetails, onBack, onConfirm }: ReviewProps) => {
   const deployFee = 0.0528; // This could be fetched from an API or config
 
   return (
@@ -74,7 +74,7 @@ const Review = ({ squadDetails, memberDetails, onBack, onConfirm }: ReviewProps)
           <svg className="w-4 h-4 inline mr-1 -mt-1" fill="currentColor" viewBox="0 0 20 20">
             <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
           </svg>
-          This amount consists of 0.05 SOL one-time platform fee, 0.001 SOL which will be deposited into your Squad's account and ~0.0018 SOL network rent needed for account deployment.
+          This amount consists of 0.05 SOL one-time platform fee, 0.001 SOL which will be deposited into your Squad&apos;s account and ~0.0018 SOL network rent needed for account deployment.
         </p>
 
         {/* Action Buttons */}
